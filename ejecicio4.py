3f0068d68e00e43ee0ab42f573a15f69f00500eb
Diego89411 <diegoalejosanchez123@gmail.com>
2022-03-18 11:56:40 -0500

    actualizacion2
    se realizaron los ejercicios vistos en clase

---
+++ ejecicio4.py
@@ -0,0 +1,5 @@
+capital=10000
+interes=0.035
+años=7
+valor=capital+(1 +(0.01 *interes)**años)
+print("la cantidad de dinero es:",(valor,12))
---
+++ ejercicio2.py
@@ -0,0 +1,7 @@
+from xml.etree.ElementTree import PI
+
+
+pi=3.14
+radio=5
+volumen=(4 / 3)* pi * (radio **3)
+print("el radio de una esfera es:",round(volumen,3))
